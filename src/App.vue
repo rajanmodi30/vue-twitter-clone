8209219af74f7bfaf20d0c06d11e1864244cf7ae
rajan <rajanmodi30@gmail.com>
2021-06-05 09:33:11 +0530

    -added first changes for the checking of watch functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    @{{ user.username }} with {{ followers }} followers
+    <button @click="followUser">follow the dude</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+  },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        username: "rajanmodi30",
+        firstName: "rajan",
+        lastName: "modi",
+        email: "rajanmodi30@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (newFollowerCount > oldFollowerCount) {
+        console.log("you gained a follower ", newFollowerCount);
+      }
+      if (newFollowerCount > 100) {
+        alert(`more then ${newFollowerCount}  follow you dawgg`);
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +54,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-align: center;
 }
 </style>
