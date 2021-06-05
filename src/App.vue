d449257ed1930d7bed124441deecfb17f967473e
rajan <rajanmodi30@gmail.com>
2021-06-05 11:26:45 +0530

    -added tailwind a little bit of colors

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <template>
-  <div>
-    @{{ user.username }} with {{ followers }} followers
-    <button @click="followUser">follow the dude</button>
+  <div >
+    <user-component></user-component>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+import UserComponent from "@/components/UserComponent.vue";
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
-  },
-  data() {
-    return {
-      followers: 0,
-      user: {
-        username: "rajanmodi30",
-        firstName: "rajan",
-        lastName: "modi",
-        email: "rajanmodi30@gmail.com",
-        isAdmin: true,
-      },
-    };
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    },
-  },
-  mounted() {
-    this.followUser();
-  },
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (newFollowerCount > oldFollowerCount) {
-        console.log("you gained a follower ", newFollowerCount);
-      }
-      if (newFollowerCount > 100) {
-        alert(`more then ${newFollowerCount}  follow you dawgg`);
-      }
-    },
+    UserComponent,
   },
 };
 </script>
