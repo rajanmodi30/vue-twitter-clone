0c3006a591931b8df5a4b0805ff1e57486dfabc8
rajan <rajanmodi30@gmail.com>
2021-06-06 07:25:08 +0530

    -created a new component to mange the views

--- src/components/AddTweet.vue
+++ src/components/AddTweet.vue
@@ -1,7 +1,46 @@
-<template> </template>
+<template>
+  <div
+    class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 float-center"
+  >
+    <form @submit.prevent="submitTweet">
+      <label>Add Tweet {{ characterCount }} / 180</label>
+      <textarea
+        v-model="tweetTextArea"
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        type="text"
+      ></textarea>
+
+      <button
+        type="submit"
+        class="float-center bg-transparent hover:bg-purple-500 text-purple-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Tweet
+      </button>
+    </form>
+  </div>
+</template>
 
 <script>
 export default {
   name: "AddTweet",
+  emits: "submitTweet",
+  data() {
+    return {
+      tweetTextArea: "",
+    };
+  },
+  methods: {
+    submitTweet() {
+      if (this.tweetTextArea) {
+        this.$emit("submitForm", this.tweetTextArea);
+        this.tweetTextArea = "";
+      }
+    },
+  },
+  computed: {
+    characterCount() {
+      return this.tweetTextArea.length;
+    },
+  },
 };
 </script>
--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -20,24 +20,7 @@
       </button>
     </div>
   </div>
-  <div
-    class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 float-center"
-  >
-    <form @submit.prevent="submitTweet">
-      <textarea
-        v-model="tweetTextArea"
-        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        type="text"
-      ></textarea>
-
-      <button
-        type="submit"
-        class="float-center bg-transparent hover:bg-purple-500 text-purple-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-      >
-        Tweet
-      </button>
-    </form>
-  </div>
+  <add-tweet @submitForm="submitTweet"></add-tweet>
   <br />
   <tweet-component
     v-for="tweet in user.tweets"
@@ -48,13 +31,13 @@
 </template>
 
 <script>
+import AddTweet from "./AddTweet.vue";
 import TweetComponent from "./TweetComponent.vue";
 export default {
   name: "UserComponent",
-  components: { TweetComponent },
+  components: { TweetComponent, AddTweet },
   data() {
     return {
-      tweetTextArea: "",
       followers: 0,
       user: {
         username: "rajanmodi30",
@@ -84,20 +67,18 @@ export default {
     toggleFavorite(id) {
       console.log(id);
     },
-    submitTweet() {
-      console.log(this.tweetTextArea);
-      if (this.tweetTextArea) {
-        this.user.tweets.unshift({
-          id: this.user.tweets.length + 1,
-          content: this.tweetTextArea,
-        });
-        this.tweetTextArea = "";
-      }
+    submitTweet(tweetTextArea) {
+      this.user.tweets.unshift({
+        id: this.user.tweets.length + 1,
+        content: tweetTextArea,
+      });
     },
   },
   mounted() {
     this.followUser();
   },
+
+ 
   watch: {
     followers(newFollowerCount, oldFollowerCount) {
       if (newFollowerCount > oldFollowerCount) {
